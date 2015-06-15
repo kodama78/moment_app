14c29a034a21d60f424a50373a040113b7eff05d
Shawn Otomo <kodama78@gmail.com>
2015-06-15 07:27:21 -0700

    added basic html structure and started js function to load page from ajax call

--- index.php
+++ index.php
@@ -8,14 +8,20 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="main.js"></script>
     <link rel="stylesheet" type="text/css" href="main.css">
+    <?php 
+        $pages = [
+                    'home'=>'assets/pages/home.html',
+                    'login'=>'assets/pages/login.html',
+                    'services'=>'pages/services.php',
+                    'contact'=>'pages/contact.php',
+                ];
+    ?>
 </head>
 
 <body>
     <header class="col-xs-12 col-md-12">
-        <ul class="nav nav-tabs" role="tablist" id="myTab">
-            <li role="presentation" class="col-xs-6 col-md-6 nopadding active"><a href="#thoughts" aria-controls="thoughts" role="tab" data-toggle="tab">Thoughts</a></li>
-            <li role="presentation" class="col-xs-6 col-md-6 nopadding"><a href="#recollections" aria-controls="recollections" role="tab" data-toggle="tab">Recollections</a></li>
-        </ul>
+    <ul class="navbar-nav"></ul>
+       
     </header>
     <div class="main_body col-xs-12 col-md-12">
     	
--- main.js
+++ main.js
@@ -1,3 +1,62 @@
-$(document.ready(function()){
-	
+/*
+These are used to create the menu links that are located in the header.
+Creating this dynamically in case menu needs to be changed at a later date.
+*/
+menu_links = {
+	'login':{
+		text: 'login',
+		file: 'login.html',
+	}
+	'new user':{
+		text: 'New User?',
+		file: 'user_create.html'
+	}
+	'home':{
+		text: 'home',
+		file: 'home.html'
+		default: true
+	}
+}
+/*
+This function is used to create the list items for the links above. 
+Created in list format for bootstrap.
+*/
+function attach_links(){
+	for (key in menu_links){
+		var li = $('<li>');
+		var a = $('<a>',{
+			text:menu_links[key].text,
+			class: 'nav_bar_link_style'
+		}).click(function{
+			load_page($(this).text());
+		});
+		li.append(a);
+		$('.navbar-nav').append(li);
+		if(typeof menu_links[key].default != 'undefined'){
+			a.click();
+		}
+	}
+}
+/*
+this function contains the ajax call that is used in the attach_links function
+above. It appends the content from each page to the main_body of the 
+index.php page.
+*/
+function load_page(page){
+	target_url = menu_links[page].file;
+	$.ajax({
+		url: 'pages/'+target_url,
+		dataType: 'html',
+		cache: false,
+		success: function(response){
+			$('.main_body').html('');
+			$('.main_body').append(response);
+		}
+	});
+		
+}
+$(document).ready(function(){
+	$('#login').click(function(){
+		load_page();
+	});
 });
