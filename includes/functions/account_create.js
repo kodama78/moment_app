1cefc472c88733069ae8cf4b17e80907d608ead0
Shawn Otomo <kodama78@gmail.com>
2015-07-02 11:22:28 -0700

    started new user creation. Need to do regex and other checks.

--- includes/functions/account_create.js
+++ includes/functions/account_create.js
@@ -5,42 +5,45 @@ CURRENT CODE!
 */
 function account_create() {
 	console.log('ACCOUNT CREATE CLICKED!');
-    // user_account.username = $('#username').val();
-    // user_account.password = $('#password').val();
-    // user_account.password_confirmation = $('#password_confirmation').val();
-    // user_account.firstname = $('#firstname').val();
-    // user_account.lastname = $('#lastname').val();
-    // user_account.email = $('#email').val();
-    // //----------------------*NOTE* ---------------------//
-    // //we need more conditionals but we are just getting this up and running for now.// 
-    // //we still need conditionals for the username use, first & last name minimum charachters//
-    // //email validation//
-    // //password must be valid//
-    // //password confirmation conditional//
-    // if (user_account.password_confirmation === user_account.password) {
-    //     console.log('passwords match!');
-    //     $.ajax({
-    //     url: 'account_create_handler.php',
-    //     dataType: 'JSON',
-    //     cache: false,
-    //     crossDomain: true,
-    //     method: 'POST',
-    //     data: {
-    //         username: user_account.username,
-    //         password: user_account.password,
-    //         email: user_account.email,
-    //         firstName: user_account.firstname,
-    //         lastName: user_account.lastname
-    //     },
-    //     success: function(response){
-    //     	console.log('response for account_create is', response);
-    //     }
-    // });
-    // }
-    // else if (user_account.password_confirmation != user_account.password) {
-    //     console.log('the passwords do not match!');
-    //     user_account = {};
-    //     $('#password').val('');
-    //     $('#password_confirmation').val('');
-    // }
+    user_account = {};
+    user_account.username = $('#username').val();
+    user_account.password = $('#password').val();
+    user_account.password_confirmation = $('#password_confirmation').val();
+    user_account.firstname = $('#firstname').val();
+    user_account.lastname = $('#lastname').val();
+    user_account.email = $('#email').val();
+    //----------------------*NOTE* ---------------------// 
+    //we still need conditionals for the username use, first & last name minimum charachters//
+    //email validation//
+    //password must be valid//
+    //password confirmation conditional//
+    if (user_account.password_confirmation === user_account.password) {
+        console.log('passwords match!');
+        $.ajax({
+            url: 'includes/functions/account_create_handler.php',
+            dataType: 'JSON',
+            cache: false,
+            crossDomain: true,
+            method: 'POST',
+            data: {
+                username: user_account.username,
+                password: user_account.password,
+                email: user_account.email,
+                first_name: user_account.firstname,
+                last_name: user_account.lastname
+            },
+            success: function(response){
+            	console.log('response for account_create is', response);
+                if(response.success == false){
+                    console.log('Sorry, user not created, ' + response.output.message);
+                }
+            }
+        });
+    }   
+    else if (user_account.password_confirmation != user_account.password) {
+        console.log('the passwords do not match!');
+        user_account = {};
+        $('#password').val('');
+        $('#password_confirmation').val('');
+    }
 }
--- includes/functions/account_create_handler.php
+++ includes/functions/account_create_handler.php
@@ -4,13 +4,28 @@
 	$username = $_POST['username'];
 	$password = sha1($_POST['password']);
 	$email = $_POST['email'];
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
-	$query = "SELECT id FROM `users` WHERE username = '$username' AND password='$password'";
-	$result = mysqli_query($conn, $query);
-	// if(mysqli_num_rows($result) > 0){
-	// 	while($inner_result = mysqli_fetch_assoc($result)){
-			
-	// 	}	
-	// }
+	$firstname = $_POST['first_name'];
+	$lastname = $_POST['last_name'];
+	$output['success'] = false;
+	$check_user_exist = "SELECT id FROM `users` WHERE username = '$username'";
+	$user_result = mysqli_query($conn, $check_user_exist);
+	
+	if(mysqli_num_rows($user_result) > 0)
+	{
+		$output['message'] = 'Sorry, that username is taken';
+		echo json_encode($output);
+	}
+	else
+	{
+		$check_user_email = "SELECT id FROM `users` WHERE email = '$email'";
+		$email_result = mysqli_query($conn, $check_user_email);
+		if(mysqli_num_rows($email_result) > 0){
+			$output['message'] = 'Sorry, an account for that email already exists.';
+			echo json_encode($output);
+		}
+		// else
+		// {
+		// 	// $input_new_user = 
+		// }
+	}
 ?>
